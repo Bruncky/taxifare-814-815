c10352e95ebe96b4bda0049d698fdbfdd03b3102
Bruno A. Brunckhorst <brunobrunckhorst@gmail.com>
2022-02-22 20:42:20 +0100

    Added method to get a pipeline with a model

---
+++ taxifare/pipeline.py
@@ -0,0 +1,28 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.compose import ColumnTransformer
+
+from taxifare.transformers.distance_transformer import DistanceTransformer
+
+def get_pipeline(model):
+    pipe_distance = make_pipeline(
+        DistanceTransformer(),
+        StandardScaler()
+    )
+
+    columns = ['pickup_latitude', 'pickup_longitude', 'dropoff_latitude', 'dropoff_longitude']
+
+    feateng_blocks = [
+        ('distance', pipe_distance, columns),
+    ]
+
+    features_encoder = ColumnTransformer(feateng_blocks)
+
+    pipeline = Pipeline(
+        steps=[
+            ('features', features_encoder),
+            ('model', model)
+        ]
+    )
+
+    return pipeline
