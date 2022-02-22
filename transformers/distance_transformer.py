da51e203705317e48f7269efffa6ec2ceb733205
Bruno A. Brunckhorst <brunobrunckhorst@gmail.com>
2022-02-22 23:01:36 +0100

    Moved transformers to separate module to fix error

--- transformers/distance_transformer.py
+++ transformers/distance_transformer.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 from sklearn.base import BaseEstimator, TransformerMixin
 
-from taxifare.transformers.utils import minkowski_distance
+from transformers.utils import minkowski_distance
 
 class DistanceTransformer(BaseEstimator, TransformerMixin):
 
