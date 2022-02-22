1e2961867b7a5386da5f74d38671ff14b1c98ae6
Bruno A. Brunckhorst <brunobrunckhorst@gmail.com>
2022-02-22 20:42:31 +0100

    Created custom transformer

---
+++ taxifare/transformers/distance_transformer.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+from taxifare.transformers.utils import minkowski_distance
+
+class DistanceTransformer(BaseEstimator, TransformerMixin):
+
+    def __init__(self, distance_type = 'euclidian', **kwargs):
+        self.distance_type = distance_type
+
+    def transform(self, X, y = None):
+        # Guard clause
+        assert isinstance(X, pd.DataFrame)
+        
+        # Handling two different types of distance
+        if self.distance_type == 'euclidian':
+            X['distance'] = minkowski_distance(X, p = 2)
+
+        if self.distance_type == 'manhattan':
+            X['distance'] = minkowski_distance(X, p = 1)
+
+        return X[['distance']]
+
+    def fit(self, X, y = None):
+        return self
