5500eb000c6bd99f89583d8336d67d6d79103363
Bruno A. Brunckhorst <brunobrunckhorst@gmail.com>
2022-02-22 20:57:59 +0100

    Added a model_name param; Added LinReg

--- taxifare/model.py
+++ taxifare/model.py
@@ -1,12 +1,19 @@
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
 
-def get_model():
-    model_params = {
-        'n_estimators': 100,
-        'max_depth': 1
-    }
+def get_model(model_name):
+    if model_name == 'random_forest':
+        model_params = {
+            'n_estimators': 100,
+            'max_depth': 1
+        }
 
-    model = RandomForestRegressor()
-    model.set_params(**model_params)
+        model = RandomForestRegressor()
+        model.set_params(**model_params)
 
-    return model
+        return model
+
+    elif model_name == 'linear_regression':
+        model = LinearRegression()
+
+        return model
