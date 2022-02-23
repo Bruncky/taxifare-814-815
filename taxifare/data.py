fa401907093bd1835f713d4bd74b1ff6990f1f70
Bruno A. Brunckhorst <brunobrunckhorst@gmail.com>
2022-02-23 18:13:40 +0100

    Modified get_data; Added save_to_gcp

--- taxifare/data.py
+++ taxifare/data.py
@@ -1,9 +1,20 @@
 import pandas as pd
+import datetime
+
+from google.cloud import storage
 
 from sklearn.model_selection import train_test_split
 
-def get_data(line_count):
-    url = 's3://wagon-public-datasets/taxi-fare-train.csv'
+def get_data(line_count, method = 's3'):
+    url = ''
+
+    if method == 's3':
+        url = 's3://wagon-public-datasets/taxi-fare-train.csv'
+    elif method == 'gs':
+        url = 'gs://le-wagon-data/data/train_1k.csv'
+    elif method == 'blob':
+        return get_data_using_blob(line_count)
+
     data = pd.read_csv(url, nrows = line_count)
 
     return data
@@ -55,3 +66,59 @@ def holdout(data):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .1)
 
     return X_train, X_test, y_train, y_test
+
+def save_model_to_gcp(cloud_model_name, local_model_path):
+    BUCKET_NAME = 'batch-814-815-berlin-bucket'
+
+    # Defining some variables for the cloud name
+    today = datetime.datetime.today()
+    date = f'{today.day}_{today.month}_{today.year}'
+    time = f'{today.hour}_{today.minute}_{today.second}'
+
+    # Name of the file ON THE CLOUD
+    storage_location = f'models/{cloud_model_name}_{date}_{time}.joblib'
+
+    # Name of LOCAL file to be uploaded
+    local_model_filename = local_model_path
+
+    # IMPORTANT: this verifies $GOOGLE_APPLICATION_CREDENTIALS
+    client = storage.Client()
+
+    # Load the bucket
+    bucket = client.bucket(BUCKET_NAME)
+
+    # Create blob to upload
+    blob = bucket.blob(storage_location)
+    blob.upload_from_filename(local_model_filename)
+
+# -------------------- HELPER METHODS -------------------- #
+def get_data_using_blob(line_count):
+    # Get data from MY OWN Google Storage bucket
+    BUCKET_NAME = 'batch-814-815-berlin-bucket'
+    BUCKET_TRAIN_DATA_PATH = 'data/train_1k.csv'
+
+    data_file = 'train_1k.csv'
+
+    # IMPORTANT: this verifies $GOOGLE_APPLICATION_CREDENTIALS
+    client = storage.Client()
+
+    # Load the bucket
+    bucket = client.bucket(BUCKET_NAME)
+
+    # Create a blob to download
+    blob = bucket.blob(BUCKET_TRAIN_DATA_PATH)
+    blob.download_to_filename(data_file)
+
+    # Load downloaded data into a DataFrame
+    data = pd.read_csv(data_file, nrows = line_count)
+
+    return data
+
+def main():
+    cloud_model_name = 'random_forest_regressor'
+    local_model_path = '/Users/bruncky/lecture_packages/taxifare/taxifare/models/random_forest.joblib'
+
+    save_model_to_gcp(cloud_model_name, local_model_path)
+
+if __name__ == '__main__':
+    main()
